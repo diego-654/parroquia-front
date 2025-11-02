8232020a01b2fa960f89b7f34742a0c221f29dc3
Lerhox <116003501+Lerhox@users.noreply.github.com>
2025-11-01 20:04:58 -0500

    Sidebar
    Se realiza sidebar, solo visual

---
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { MenuItem } from './sidebar.interface';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss',
+})
+
+export class Sidebar {
+  title = 'PARROQUIA LA PURISIMA\nCONCEPCION';
+
+  menuPartidas: MenuItem[] = MENU_PARTIDAS;
+  menuNotificacion: MenuItem[] = MENU_NOTIFICACION;
+
+  showPartidas = true;
+  showNotificacion = true;
+
+  onLogout() {
+    console.log('Cerrar Sesión');
+  }
+}
+
+
+export const MENU_PARTIDAS: MenuItem[] = [
+  { 
+    label: 'Bautizo', 
+    url: '/bautizo' },
+  { 
+    label: 'Matrimonio', 
+    url: '/matrimonio' 
+  },
+  { 
+    label: 'Primera Comunión', 
+    url: '/primera-comunion' 
+  },
+  { 
+    label: 'Confirmación', 
+    url: '/confirmacion' 
+  },
+];
+
+export const MENU_NOTIFICACION: MenuItem[] = [
+  { 
+    label: 'Matrimonio', 
+    url: '/notificacion/matrimonio' 
+  },
+  { 
+    label: 'Proclama', 
+    url: '/notificacion/proclama' 
+  },
+];
